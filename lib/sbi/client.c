81d073deb4fe60579a9904f92c2a323cbd9068a8
sai <go29ben@mytum.de>
2025-10-06 15:23:16 +0000

    P-256

--- lib/sbi/client.c
+++ lib/sbi/client.c
@@ -388,8 +388,8 @@ static CURLcode sslctx_callback(CURL *curl, void *sslctx, void *userdata)
     ogs_assert(ctx);
     ogs_assert(userdata);
 
-    SSL_CTX_set1_groups_list(ctx, "X25519MLKEM768");
-    ogs_info("Key exchange with: Hybrid X25519 - MLKEM768");
+    SSL_CTX_set1_groups_list(ctx, "P-256");
+    ogs_info("Key exchange with: P-256 ECDH");
 
     return CURLE_OK;
 }
